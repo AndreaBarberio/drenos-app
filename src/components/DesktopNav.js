d30e8a9e8c6a56512792822dc822209322ef95f4
AndreaBarberio <andrea.barberio97@gmail.com>
2025-07-05 17:47:31 +0200

    style(navbar): fix li positioning

--- src/components/DesktopNav.js
+++ src/components/DesktopNav.js
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom";
 
 
 
-const imgStyle = "self-center align-center h-8 w-8 rounded-md shadow-black shadow-md"
+const imgStyle = "self-center align-center h-8 w-8 rounded-md shadow-black shadow-md ml-2"
 
 function DesktopNav({ logo: { src, alt }, links, onCartClick, userMenu, isLoggedIn }) {
   links.map((link) => console.log("link", link))
 
   return (
-    <nav className="h-12 w-full bg-slate-400 text-gray-500" aria-label="Main navigation">
+    <nav className="flex flex-initial h-12 w-full bg-slate-400 text-gray-500" aria-label="Main navigation">
       <img className={imgStyle} src={src} alt={alt} />
-      <ul>
+      <ul className="ml-2 flex flex-row flex-initial self-center gap-2" >
         {links.map((link, i) => (
           <li key={i}>
             <Link to={link.path}>
