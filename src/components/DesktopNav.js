6afb9db4abfeddbe28103879f39ba7124b580cf8
AndreaBarberio <andrea.barberio97@gmail.com>
2025-07-06 18:57:16 +0200

    feat(ui): add shadow and 404 asset

--- src/components/DesktopNav.js
+++ src/components/DesktopNav.js
@@ -8,11 +8,11 @@ function DesktopNav({ logo: { src, alt }, links, onCartClick, userMenu, isLogged
   links.map((link) => console.log("link", link))
 
   return (
-    <nav className="flex h-12 w-full bg-transparent text-gray-300" aria-label="Main navigation">
+    <nav className="flex h-12 w-full bg-transparent text-gray-100  " aria-label="Main navigation">
       <img className={imgStyle} src={src} alt={alt} />
       <ul className="flex flex-1 justify-evenly ml-2  flex-row  self-center" >
         {links.map((link, i) => (
-          <li key={i}>
+          <li className="text-shadow-lg" key={i}>
             <Link to={link.path}>
               {link.meta?.label}
             </Link>
