1914009d8acd12c8097ead28e752b967c674d085
AndreaBarberio <andrea.barberio97@gmail.com>
2025-07-07 14:49:49 +0200

    feat(ui): initialized subcategories pages and 404 created and configured

---
+++ src/pages/Products/Notebook/Notebook.js
@@ -0,0 +1,9 @@
+function Notebook() {
+  return (
+    <div className="text-white text-xl">
+      <h1 className="text-3xl font-bold mb-4">Notebook</h1>
+      <p>This page will show our Notebooks</p>
+    </div>
+  );
+}
+export default Notebook;
