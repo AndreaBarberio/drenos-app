18b85c78a73870da737e53a5e62430f2fbff7283
AndreaBarberio <andrea.barberio97@gmail.com>
2025-07-12 23:01:06 +0200

    feat(mobnav): initialize dropdown component to mobile

--- src/components/Navbar/Dropdown.js
+++ src/components/Navbar/Dropdown.js
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom";
 
 function Dropdown({ labels, urls }) {
-
   return (
-    <div className="absolute pt-2">
-      <ul className="dropdown-container">
+    <div
+      className=" w-full fixed bottom-0 left-0  bg-white border-t border-gray-300 md:absolute md:top-full md:left-0 md:w-max md:bg-transparent md:border-none md:pt-2"
+
+    >
+      <ul className="flex md:flex-col justify-around md:justify-start p-2 md:p-0">
         {urls.map((url, i) => (
-          <Link to={url} key={i}>
-            <li className="shadow-md shadow-white p-2 rounded-xl bg-gradient-to-b from-slate-400 hover:text-blue-600 hover:shadow-blue-400 transition ">{labels[i]}</li>
+          <Link to={url} key={i} className="w-full">
+            <li className="opacity-80 hover:opacity-100 text-center md:text-left shadow-md shadow-white p-2 rounded-xl bg-gradient-to-b from-[rgb(129,155,167)] to-[rgb(172,194,204)] hover:text-blue-600 hover:shadow-blue-400 transition w-full">
+              {labels[i]}
+            </li>
           </Link>
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
 export default Dropdown;
